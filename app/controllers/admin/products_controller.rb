d3ae9884ef7ac47c73b11728b4d9ca2d88ba6910
Erin Von Gertzen <ianvon@shaw.ca>
2024-04-03 14:42:58 -0600

    mergeded order details

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
-
+  #http_basic_authenticate_with name: ENV["ADMIN_LOGIN"], password: ENV["ADMIN_PASS"]
+  http_basic_authenticate_with name: "Jungle", password: "book"
   def index
     @products = Product.order(id: :desc).all
   end
