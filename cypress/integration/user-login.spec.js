4a20590c489cacdc1f5a61f4c334b8b8af01702f
Erin Von Gertzen <ianvon@shaw.ca>
2024-04-08 16:35:01 -0600

    Created test page for feature user login and fix some other issues

---
+++ cypress/integration/user-login.spec.js
@@ -0,0 +1,58 @@
+describe('Sign Up', () => {
+
+  it('visit the main page', () => {
+    cy.visit('/');
+  });
+
+  it('goes to sign up page', () => {
+    cy.get('.navbar-nav > :nth-child(1) > .btn').click();
+    cy.contains('Sign up');
+  });
+
+  it('fill in first name', () => {
+    cy.get('#user_first_name').type('Sarah', { force: true });
+  });
+
+  it('fill in last name', () => {
+    cy.get('#user_last_name').type('Jane', { force: true });
+  });
+
+  it('fill in email', () => {
+    cy.get('#user_email').type('sarahj@gmail.com', { force: true });
+  });
+
+  it('fill in password', () => {
+    cy.get('#user_password').type('sarahj', { force: true });
+  });
+
+  it('fill in password confirmation', () => {
+    cy.get('#user_password_confirmation').type('sarahj', { force: true });
+  });
+
+  it('click sign up button', () => {
+    cy.get(':nth-child(6) > .btn').click();
+  });
+
+});
+
+describe('Sign in', () => {
+
+  it('goes to sign in page', () => {
+    cy.get(':nth-child(2) > .btn').click();
+    cy.contains('Sign In');
+  });
+
+  it('fill in email', () => {
+    cy.get('#email').type('sarahj@gmail.com', { force: true });
+  });
+
+  it('fill in password', () => {
+    cy.get('#password').type('sarahj', { force: true });
+  });
+
+  it('click sign in button', () => {
+    cy.get(':nth-child(3) > .btn').click();
+    cy.contains('Logged in successfully');
+  });
+
+});
